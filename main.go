47be227efd2972d0401cd43ab3c259ffe3027955
heliang <heliang_827@163.com>
2020-04-13 12:00:34 +0800

    希尔排序,重新学习(设置多大的间隔,就会将序列分隔成多少个子序列,子序列间进行插入排序,直到序列间隔为1[此时相当于原始的插入排序,但是现在的数据已经基本有序])

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 //对排序不是很了解,学学
 
 func main() {
-	selectData := []int{8, 21, 3, 4, 5, 6}
-	sorts.InertSort(selectData)
+	selectData := []int{8, 21, 3, 4, 5, 6, 88, 1, 28, 0, 42, 34, 29, 19, 85, -1, 81, 92, 101, 8888, 9999, -1002}
+	sorts.ShellSort(selectData)
 	fmt.Println(selectData)
 }
