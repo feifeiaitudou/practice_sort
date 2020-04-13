8e6e093084fbd9cbbc7effb7dca85d6aed0ce39d
heliang <heliang_827@163.com>
2020-04-13 13:55:32 +0800

    希尔排序修正(实现真正对不同的间隔下的每个子序列进行插入排序)

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 //对排序不是很了解,学学
 
 func main() {
-	selectData := []int{8, 21, 3, 4, 5, 6, 88, 1, 28, 0, 42, 34, 29, 19, 85, -1, 81, 92, 101, 8888, 9999, -1002}
+	selectData := []int{29, 19, 85, -1, 8888, 81, 92, 101, 9999, -1002, 8, 21, 3, 4, 5, 6, 88, 1, 28, 0, 42, 34}
 	sorts.ShellSort(selectData)
 	fmt.Println(selectData)
 }
